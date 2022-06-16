45f7f51adca3a8e541957c52cc2c764a2b5d76ae
Abidijihed <abidij55@gmail.com>
2022-06-16 09:09:13 +0100

    save

--- file.js
+++ file.js
@@ -1,16 +1,23 @@
 /*
- * Given an array of numbers, calculate the greatest contiguous sum of numbers in it.
- * A single array item will count as a contiguous sum.
- *
- * example 1: sumArray([1, 2, 3]); // => 6
- * example 2: sumArray([1, 2, 3, -4]); // 6
- * example 3: sumArray([1, 2, 3, -4, 5]); // 7
- * example 4: sumArray([4, -1, 5]); // => 8
- * example 5: sumArray([10, -11, 11]); // 11
- */
+In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+1p piece
+2p piece
+5p piece
+10p piece
+20p piece
+50p piece
+£1 (100p)
+£2 (200p)
+It is possible to make £2 in the following way:
+1 * £1 + 1 * 50p + 2 * 20p + 1 * 5p + 1 * 2p + 3 * 1p
+How many different ways can £2 be made using any number of coins?
+example usage of `makeChange`:
+// aka, there's only one way to make 1p. that's with a single 1p piece
+makeChange(1) === 1
+// aka, there's only two ways to make 2p. that's with two, 1p pieces or with a single 2p piece
+makeChange(2) === 2
+*/
 
-// Solved in O(n) time with O(1) space
-var sumArray = function(array) {
-   //code
- };
- 
+var makeChange = function(total){
+
+};
