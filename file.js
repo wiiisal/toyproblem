b523d6391469087188807b70fa628f46db519e07
Abidijihed <abidij55@gmail.com>
2022-06-27 12:08:29 +0100

    save stack

--- file.js
+++ file.js
@@ -1,5 +1,29 @@
-// Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
 
-var isPowerOfFour = function() {
- 
+var Stack = function() {
+ var storage=[]
+ var length=0
+ this.push=function(value){
+
+ }
+ this.pop=function(){
+
+ }
+ this.size=function(){
+
+ }
 };
+
+var Queue=function(){
+    var inbox=new Stack()
+    var outbox =new Stack()
+
+    this.enqueue=function(){
+
+    }
+    this.dequeue=function(){
+
+    }
+    this.size=function(){
+        
+    }
+}
