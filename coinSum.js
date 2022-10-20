eb91d7dc88299d376e56206f79e7065b3d449138
Abidijihed <abidij55@gmail.com>
2022-10-20 09:36:14 +0100

    save

--- coinSum.js
+++ coinSum.js
@@ -18,11 +18,25 @@ In other words, find all the possible combinations of coins that sum to a given 
 */
 
 // HELPERS
-var coins = [1, 2, 5, 10, 20, 50, 100, 200];
+var coins = [1,2,5,10,20,50,100,200];
+function coinSums (total) {
+  var sums = 0;  // this is for keeping track of all the possibilities
+  function innerRecursion(recurseIndex, leftover){
+    var current = coins[recurseIndex];
 
-function coinSums(total) {
-  // your code here...
+    if (recurseIndex === 0){
+      if (leftover % current === 0){
+            
 
-
-
+        sums++;
+      }
+      return;
+    }
+    while (leftover >= 0){
+      innerRecursion(recurseIndex - 1, leftover);
+      leftover -= current;
+    }
+  }
+  innerRecursion(coins.length - 1, total);
+  return sums;
 }
