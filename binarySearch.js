427814ca3434747fff7029d4ae65467af6c19e8b
Wiiisal <wissalbensai011@gmail.com>
2022-10-25 14:37:55 +0100

    save

--- binarySearch.js
+++ binarySearch.js
@@ -9,12 +9,6 @@
  */
 
 var binarySearch = function(arr, element) {
-<<<<<<< HEAD
-for (let i=0; i< arr.length ;i++){
-   return arr[i];
-}
-=======
-       return arr.indexOf(element)
->>>>>>> be6f2260c0b14d0b0407764ce0fb2adf7aea4a7c
+    return arr.indexOf(element)
 }
  
