625fbb65f4c875956de234e81cb5929e4b5cec17
Wiiisal <wissalbensai011@gmail.com>
2022-11-07 09:17:41 +0100

    nouveau dossier

--- mergesort.js
+++ mergesort.js
@@ -55,7 +55,21 @@
  */
 
 // RECURSIVE (top-down) approach
-var mergeSort = function(arr) {
-  
+function merge(left, right) {
+  let sortedArr = [] 
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift())
+    } else {
+      sortedArr.push(right.shift())
+    }
   }
-  
+  return [...sortedArr, ...left, ...right]
+}
+var mergeSort = function(arr) {
+    if (arr.length <= 1) return arr
+    let mid = Math.floor(arr.length / 2)
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+    return merge(left, right)
+  }
