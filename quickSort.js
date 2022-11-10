dd20641d8da8029fef1928b41cbf82a2dedf6bce
Abidijihed <abidij55@gmail.com>
2022-11-10 09:24:32 +0100

    save

--- quickSort.js
+++ quickSort.js
@@ -18,6 +18,13 @@ NOTE: DO NOT use JavaScript’s built-in sorting function (Array.prototype.sort)
 */
 
 function quickSort(arr) {
- 
-  
+ if(arr.length <= 1)return arr
+ var pivot=arr[Math.floor(arr.length/2)],left=[],right=[]
+ for(var i=0;i<arr.length;i++){
+  if(i===Math.floor(arr.length/2))continue
+  if(arr[i]<pivot)left.push(arr[i])
+  else right.push(arr[i])
+ }
+  var sorted=quickSort(left).concat(pivot,quickSort(right));
+  return sorted
   }
